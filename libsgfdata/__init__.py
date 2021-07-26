7821f6912e60a79b866ba7bef9fe8bc4e582f709
Egil <egil.moller@freecode.no>
2021-07-26 15:07:31 +0200

    Support for writing sgf files

--- libsgfdata/__init__.py
+++ libsgfdata/__init__.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 import slugify
 import codecs
+import copy
 
 blocknames = {"£": "method", "$":"main", "#":"data", "€": "method"}
+unblocknames = {v:k for k, v in blocknames.items()}
 
 def _read_csv(f):
     return pd.read_csv(f, na_values=['', '#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan',
@@ -31,6 +33,11 @@ data["ident"] = data.name.astype(str).apply(lambda x: slugify.slugify(x, separat
 methods["ident"] = methods.name.astype(str).apply(lambda x: slugify.slugify(x, separator="_"))
 comments["ident"] = comments.name.astype(str).apply(lambda x: slugify.slugify(x, separator="_"))
 
+unmethod = method.reset_index().set_index("ident")
+unmain = main.reset_index().set_index("ident")
+undata = data.reset_index().set_index("ident")
+unmethods = methods.reset_index().set_index("ident")
+uncomments = comments.reset_index().set_index("ident")
 
 _RE_FLOAT = re.compile(r"^[-+]?[0-9]*(\.[0-9]*)?(e[-+]?[0-9]+)?$")
 _RE_INT = re.compile(r"^[-+]?[0-9]+$")
@@ -131,3 +138,82 @@ def parse(*arg, **kw):
     _rename_values_comments(sections)
     return sections
 
+
+def _dump_line(line):
+    return ",".join("%s=%s" % (k,v) for k,v in line.items() if str(v) and (not isinstance(v, float) or not np.isnan(v)))
+
+def _dump_raw(sections, output_filename=None, *arg, **kw):
+    if isinstance(output_filename, str):
+        with open(output_filename, "rb") as f:
+            _dump_raw_to_file(sections, f, *arg, **kw)
+    elif output_filename is not None:
+        _dump_raw_to_file(sections, output_filename, *arg, **kw)
+    else:
+        raise NotImplementedError
+
+def _dump_raw_to_file(sections, f, encoding="latin-1"):
+    f = codecs.getwriter(encoding)(f, errors='ignore')
+    for section in sections:
+        for blockname in ("$", "£", "#", "€", "#$"):
+            if blockname == "$" or (blockname in section and section[blockname]):
+                f.write(blockname + "\n")
+                for row in section.get(blockname, []):
+                    f.write(_dump_line(row) + "\n")
+
+def _unrename_blocks(sections):
+    for idx in range(len(sections)):
+        sections[idx] = {unblocknames.get(name, name): block
+                         for name, block in sections[idx].items()}
+
+def _unmake_dfs(sections):
+    for idx in range(len(sections)):
+        if "data" in sections[idx]:
+            sections[idx]["data"] = sections[idx]["data"].to_dict('records')
+
+def _unrename_data_columns(sections):
+    for idx in range(len(sections)):
+        if "data" in sections[idx]:
+            sections[idx]["data"] = sections[idx]["data"].rename(columns = undata.code.to_dict())
+
+def _unrename_main(sections):
+    for idx in range(len(sections)):
+        sections[idx]["main"] = [
+            {unmain.loc[key, "code"] if key in unmain.index else key: value
+             for key, value in row.items()}
+            for row in sections[idx]["main"]]
+
+def _unrename_method(sections):
+    for idx in range(len(sections)):
+        sections[idx]["method"] = [
+            {unmethod.loc[key, "code"] if key in unmethod.index else key: value
+             for key, value in row.items()}
+            for row in sections[idx]["method"]]
+        
+def _unrename_values_method_code(sections):
+    for section in sections:
+        for row in section["main"]:
+            if 'method_code' in row:
+                code = str(row['method_code'])
+                if code in unmethods.index:
+                    row['method_code'] = unmethods.loc[code, "code"]
+
+def _unrename_values_comments(sections):
+    for section in sections:
+        if "comments" in section["data"].columns:
+            codes = section["data"].comments
+            missing = list(set(codes.unique()) - set(uncomments.index))
+            labels = pd.concat((uncomments,
+                                pd.DataFrame([{"code": code} for code in missing], index=missing)))
+            section["data"]["comments"] = labels.loc[codes, "code"].values
+                    
+def dump(sections, *arg, **kw):
+    sections = copy.deepcopy(sections)    
+    _unrename_values_comments(sections)
+    _unrename_data_columns(sections)
+    _unmake_dfs(sections)
+    _unrename_method(sections)
+    _unrename_values_method_code(sections)
+    _unrename_main(sections)
+    _unrename_blocks(sections)
+    sections = _dump_raw(sections, *arg, **kw)
+    return sections
