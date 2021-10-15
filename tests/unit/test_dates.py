e71ef88cc13fa2b6c404a623679443e49a67a1cd
Egil <egil.moller@freecode.no>
2021-10-15 14:57:20 +0200

    Added test

--- tests/unit/test_dates.py
+++ tests/unit/test_dates.py
@@ -11,6 +11,7 @@ class TestDates:
         ("main", "HD", "20120105", datetime.date(2012, 1, 5)),
         ("main", "HD", "09/04/99", datetime.date(1999, 4, 9)),
         ("main", "HD", "27.06.2014", datetime.date(2014, 6, 27)),
+        ("main", "KD", "27.06.2014", datetime.date(2014, 6, 27)),
     ])
     def test_dates(self, block, key, date_string, expected_result):
         assert expected_result == sgf._conv(block, key, date_string)
