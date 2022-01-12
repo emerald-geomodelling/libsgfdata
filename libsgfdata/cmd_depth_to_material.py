9f7476dd173b5e3f1102a9040c0d918845747e5f
Egil <egil.moller@freecode.no>
2022-01-12 15:28:35 +0100

    Let the user chose comments column

--- libsgfdata/cmd_depth_to_material.py
+++ libsgfdata/cmd_depth_to_material.py
@@ -6,10 +6,11 @@ class depth_to_material(object):
     @classmethod
     def decorate(cls, cmd):
         cmd = click.option('--depth-to-material', type=str, multiple=True, help="Set depth_bedrock to shallowest depth labeled with a certain material name in the comment (multiple can be given)")(cmd)
+        cmd = click.option('--material-column', type=str, default="comments", help="Column to get material types from (comments)")(cmd)
         return cmd
 
     @classmethod
-    def transform(cls, data, depth_to_material, **kw):
+    def transform(cls, data, depth_to_material, material_column, **kw):
         for section in data:
             borehole_data = section["data"]
 
@@ -19,7 +20,7 @@ class depth_to_material(object):
                         return material
                 return None
 
-            materials = borehole_data.comments.apply(find_material)
+            materials = borehole_data[material_column].apply(find_material)
 
             depth_bedrock = np.NaN
             for cls in depth_to_material:
