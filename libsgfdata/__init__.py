9ca922678024d3747e2148d153e71c2c6b5b1a67
Egil <egil.moller@freecode.no>
2022-05-16 08:33:33 +0200

    Better geotech set structure

--- libsgfdata/__init__.py
+++ libsgfdata/__init__.py
@@ -4,33 +4,39 @@ from .dumper import dump
 from .normalizer import normalize
 import pandas as pd
 
-def sections_to_geotech_set(sections):
-    unique_ids = len(set([borehole["main"][0]["investigation_point"] for borehole in sections]))
-    assert unique_ids == len(sections), "investigation_point is not unique for each borehole"
-    
-    main = pd.DataFrame([merge_dicts(*borehole["main"]) for borehole in sections])
-    data = pd.concat([
-        borehole["data"].assign(investigation_point=borehole["main"][0]["investigation_point"])
-        for borehole in sections
-        if "data" in borehole])
-    method = pd.concat([
-        pd.DataFrame(borehole["method"]).assign(investigation_point=borehole["main"][0]["investigation_point"])
-        for borehole in sections
-        if "method" in borehole])
-    return {"main": main, "data": data, "method": method}
-
-
 def merge_dicts(*dicts):
     res = {}
     for d in dicts:
         res.update(d)
     return res
 
-def geotech_set_to_sections(geotech):
+def sections_to_geotech_set(sections, merge=False, id_col="investigation_point"):
+    unique_ids = set([borehole["main"][0][id_col] for borehole in sections])
+    assert len(unique_ids) == len(sections), "%s is not unique for each borehole" % id_col
+
+    if merge:
+        main = pd.DataFrame([merge_dicts(*borehole["main"]) for borehole in sections])
+    else:
+        main = pd.concat([
+            pd.DataFrame(borehole["main"]).assign(**{id_col: borehole["main"][0][id_col]})
+            for borehole in sections
+            if "main" in borehole])
+    
+    data = pd.concat([
+        borehole["data"].assign(investigation_point=borehole["main"][0][id_col])
+        for borehole in sections
+        if "data" in borehole])
+    method = pd.concat([
+        pd.DataFrame(borehole["method"]).assign(investigation_point=borehole["main"][0][id_col])
+        for borehole in sections
+        if "method" in borehole])
+    return {"main": main, "data": data, "method": method}
+
+def geotech_set_to_sections(geotech, id_col="investigation_point"):
     return [{"main": [row.to_dict()],
-      "data": geotech["data"][geotech["data"].investigation_point == row.investigation_point],
+      "data": geotech["data"][geotech["data"][id_col] == row[id_col]],
       "method": [method_row for method_idx, method_row
-                 in geotech["method"][geotech["method"].investigation_point == row.investigation_point].iterrows()]
+                 in geotech["method"][geotech["method"][id_col] == row[id_col]].iterrows()]
      } for idx, row in geotech["main"].iterrows()]
 
 _dump_function = dump
@@ -39,17 +45,22 @@ class SGFData(object):
     def __new__(cls, *arg, **kw):
         self = object.__new__(cls)
         if arg or kw:
+            self.id_col = kw.pop("id_col", "investigation_point")
             if arg and isinstance(arg[0], dict):
                 self.model_dict = arg[0]
             elif arg and isinstance(arg[0], list):
-                self.model_dict = sections_to_geotech_set(arg[0])
+                self.model_dict = sections_to_geotech_set(arg[0], id_col=self.id_col)
             else:
-                self.model_dict = sections_to_geotech_set(parse(*arg, **kw))
+                self.model_dict = sections_to_geotech_set(parse(*arg, **kw), id_col=self.id_col)
         return self
 
     def dump(self, *arg, **kw):
-        _dump_function(geotech_set_to_sections(self.model_dict), *arg, **kw)
+        _dump_function(self.sections, *arg, **kw)
 
+    @property
+    def sections(self):
+        return geotech_set_to_sections(self.model_dict, id_col=self.id_col)
+        
     @property
     def main(self):
         return self.model_dict["main"]
