7b370c7f00aa697e2164658f2869d590d58d8e05
Egil <egil.moller@freecode.no>
2023-04-19 15:33:50 +0200

    Normalize stop code

--- libsgfdata/normalizer.py
+++ libsgfdata/normalizer.py
@@ -1,5 +1,7 @@
 import logging
 from . import metadata
+import pandas as pd
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +30,15 @@ def normalize_coordinates(sgf, project_crs=None, **kw):
         
     sgf.main = sgf.main.groupby("projection", group_keys=False).apply(reproject)
 
+def normalize_stop_code(sgf):
+    if "comments" not in sgf.data.columns: return
+    if "stop_code" not in sgf.main.columns:
+        data.main["stop_code"] = "no_comment"
+    last_comment = sgf.main[["investigation_point"]].merge(
+        sgf.data.groupby(sgf.id_col).comments.last().rename("last_comment"),
+        left_on="investigation_point", right_index=True)
+    sgf.main["stop_code"] = np.where(pd.isnull(sgf.main.stop_code), last_comment.last_comment, sgf.main.stop_code)
+    
 def normalize_columns(sgf):
     for blockname, block in sgf.model_dict.items():
         normalization = metadata.block_metadata[blockname].loc[
@@ -39,4 +50,5 @@ def normalize_columns(sgf):
     
 def normalize(sgf, **kw):
     normalize_columns(sgf)
+    normalize_stop_code(sgf)
     normalize_coordinates(sgf, **kw)
