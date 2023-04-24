5d5cc5ca99550d77ae50f888203175a078473cb5
Egil <egil.moller@freecode.no>
2023-04-24 12:56:52 +0200

    Less fragile normalization

--- libsgfdata/normalizer.py
+++ libsgfdata/normalizer.py
@@ -16,20 +16,24 @@ def normalize_coordinates(sgf, projection=None, **kw):
     
     if projection is None:
         projection = sgf.main.projection.iloc[0]
-    
-    sgf.main = sgf.main.rename(columns={
-        "x_coordinate":"x_orig",
-        "y_coordinate":"y_orig"})
 
+    if "projection_orig" not in sgf.main.columns:
+        sgf.main["projection_orig"] = sgf.main.projection
+        sgf.main = sgf.main.rename(columns={
+            "x_coordinate":"x_orig",
+            "y_coordinate":"y_orig"})
+
+    # FIXME: Don't reproject unnecessarily
+        
     def reproject(df):
-        src = df.projection.iloc[0]
-        df["projection"] = projection
+        src = df.projection_orig.iloc[0]
         df["x_coordinate"], df["y_coordinate"] = project(src, projection, df["x_orig"].values, df["y_orig"].values)
         df["x_web"], df["y_web"] = project(src, 3857, df["x_orig"].values, df["y_orig"].values)
         df["lon"], df["lat"] = project(src, 4326, df["x_orig"].values, df["y_orig"].values)
         return df
         
-    sgf.main = sgf.main.groupby("projection", group_keys=False).apply(reproject)
+    sgf.main = sgf.main.groupby("projection_orig", group_keys=False).apply(reproject)
+    sgf.main["projection"] = projection
 
 def normalize_stop_code(sgf):
     if "comments" not in sgf.data.columns: return
@@ -37,20 +41,23 @@ def normalize_stop_code(sgf):
         data.main["stop_code"] = "no_comment"
     last_comment = sgf.main[["investigation_point"]].merge(
         sgf.data.groupby(sgf.id_col).comments.last().rename("last_comment"),
-        left_on="investigation_point", right_index=True)
+        left_on="investigation_point", right_index=True, how="left")
     sgf.main["stop_code"] = np.where(pd.isnull(sgf.main.stop_code), last_comment.last_comment, sgf.main.stop_code)
     
 def normalize_columns(sgf):
     for blockname, block in sgf.model_dict.items():
+        if blockname not in metadata.block_metadata:
+            continue
         normalization = metadata.block_metadata[blockname].loc[
             ~metadata.block_metadata[blockname].normalization.isna()
         ].set_index("ident").normalization
         normalization = metadata.block_metadata[blockname].loc[normalization].set_index(normalization.index).ident
 
         for src, dst in normalization.to_dict().items():
-            filt = ~pd.isnull(block[src]) 
-            block.loc[filt, dst] = block.loc[filt, src]
-            block.drop(columns=[src], inplace=True)
+            if src in block.columns:
+                filt = ~pd.isnull(block[src]) 
+                block.loc[filt, dst] = block.loc[filt, src]
+                block.drop(columns=[src], inplace=True)
         
         #block.rename(columns=normalization.to_dict(), inplace=True)
 
@@ -63,7 +70,7 @@ def normalize_depth(sgf):
     
     last_depth = sgf.main[["investigation_point"]].merge(
         sgf.data.groupby(sgf.id_col).depth.max().rename("last_depth"),
-        left_on="investigation_point", right_index=True)
+        left_on="investigation_point", right_index=True, how="left")
     
     sgf.main["depth_min"] = np.where(pd.isnull(sgf.main.depth_min),
                                      last_depth.last_depth,
@@ -81,16 +88,16 @@ def normalize_depth(sgf):
 
 def normalize_id(sgf):
     sgf.main[sgf.id_col] = sgf.main[sgf.id_col].astype(str)
-    sgf.data[sgf.id_col] = sgf.data[sgf.id_col].astype(str)
-    sgf.method[sgf.id_col] = sgf.method[sgf.id_col].astype(str)
+    if sgf.data is not None: sgf.data[sgf.id_col] = sgf.data[sgf.id_col].astype(str)
+    if sgf.method is not None: sgf.method[sgf.id_col] = sgf.method[sgf.id_col].astype(str)
     
 def normalize_order(sgf):
     sgf.main.sort_values([sgf.id_col], inplace=True)
     sgf.data.sort_values([sgf.id_col, "depth"], inplace=True)
 
     sgf.main.reset_index(inplace=True, drop=True)
-    sgf.data.reset_index(inplace=True, drop=True)
-    sgf.method.reset_index(inplace=True, drop=True)
+    if sgf.data is not None: sgf.data.reset_index(inplace=True, drop=True)
+    if sgf.method is not None: sgf.method.reset_index(inplace=True, drop=True)
     
 def normalize(sgf, sort=False, **kw):
     normalize_columns(sgf)
