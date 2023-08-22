6fbd3ca359e09e48125903fe7a6fd404e3b35f81
Duke-of-Lizard <cch@emeraldgeo.no>
2023-08-22 10:49:27 +0200

    check for all relevant depth columns before validation checks

--- libsgfdata/validate.py
+++ libsgfdata/validate.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 def validate_depth_min(sgf):
-    if "depth_max_drilled" not in sgf.main.columns:
-        return np.ones(len(sgf.main))
+    if "depth_max_drilled" not in sgf.main.columns or 'depth_min' not in sgf.main.columns:
+        return np.ones(len(sgf.main), bool)
     return np.isnan(sgf.main["depth_min"]) | np.isnan(sgf.main["depth_max_drilled"]) | (sgf.main["depth_min"] <= sgf.main["depth_max_drilled"])
 
 def validate_depth_depth_min(sgf):
+    if "depth_min" not in sgf.main.columns or 'depth' not in sgf.main.columns:
+        return np.ones(len(sgf.main), bool)
     return np.isnan(sgf.main["depth"]) | np.isnan(sgf.main["depth_min"]) | (sgf.main["depth"] >= sgf.main["depth_min"])
 
 def validate_depth_depth_max_drilled(sgf):
-    if "depth_max_drilled" not in sgf.main.columns:
-        return np.ones(len(sgf.main))
+    if "depth_max_drilled" not in sgf.main.columns or 'depth' not in sgf.main.columns:
+        return np.ones(len(sgf.main), bool)
     return np.isnan(sgf.main["depth"]) | np.isnan(sgf.main["depth_max_drilled"]) | (sgf.main["depth"] <= sgf.main["depth_max_drilled"])
 
 def validate(sgf):
